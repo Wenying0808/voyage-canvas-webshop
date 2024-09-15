bb675126873460dd136242055097d6b6a8b615ad
Wenying Chang <changwenyingnl@gmail.com>
2024-09-15 15:37:15 +0200

    add navbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-navbar>
     <router-outlet />
   `,
   styles: [],
---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  template: `
+    <mat-toolbar class="navbar">
+      <span class="navbar-logo">Voyage Canvas</span>
+      <span class="navbar-spacer"></span>
+      <a class="navbar-tab" routerLink="/about" routerLinkActive="active-link" [routerLinkActiveOptions]="{exact: true}">About</a>
+      <a class="navbar-tab" routerLink="/webshop" routerLinkActive="active-link" [routerLinkActiveOptions]="{exact: true}">Webshop</a>
+      <button mat-icon-button class="navbar-tab-button" aria-label="basket" routerLink="/basket" routerLinkActive="active-link" [routerLinkActiveOptions]="{exact: true}">
+          <mat-icon>shopping_bag</mat-icon>
+      </button>
+      <button mat-icon-button class="navbar-tab-button" aria-label="account" routerLink="/account" routerLinkActive="active-link" [routerLinkActiveOptions]="{exact: true}">
+          <mat-icon>account_circle</mat-icon>
+      </button>
+    </mat-toolbar>
+  `,
+  styleUrl: `./navbar.component.scss`
+})
+
+export class NavbarComponent {
+
+}
