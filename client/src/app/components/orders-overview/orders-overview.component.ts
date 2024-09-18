cc37034012c7566a0cb5f31373c745034a79400d
Wenying Chang <changwenyingnl@gmail.com>
2024-09-18 09:52:13 +0200

    add side bar in the account page

---
+++ client/src/app/components/orders-overview/orders-overview.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-overview',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      orders-overview works!
+    </p>
+  `,
+  styleUrl: `./orders-overview.component.scss`
+})
+export class OrdersOverviewComponent {
+
+}
--- client/src/app/pages/account/account.component.ts
+++ client/src/app/pages/account/account.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { AccountOverviewComponent } from '../../components/account-overview/account-overview.component';
+import { OrdersOverviewComponent } from '../../components/orders-overview/orders-overview.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, SidebarComponent, AccountOverviewComponent, OrdersOverviewComponent],
   template: `
     <div class="account">
-      <p>
-        account works!
-      </p>
-    <div>
+      <app-sidebar (tabSelected)="onTabSelected($event)"></app-sidebar>
+      <app-account-overview *ngIf="selectedTab === 'account'"></app-account-overview>
+      <app-orders-overview *ngIf="selectedTab === 'orders'"></app-orders-overview>
+    </div>
   `,
   styleUrl: `./account.component.scss`,
 })
-export class AccountComponent {
 
+export class AccountComponent {
+  selectedTab = 'account'; // Default to 'account'
+
+  onTabSelected(tab: string) {
+    console.log('Selected tab:', tab);
+    this.selectedTab = tab;
+  }
 }
