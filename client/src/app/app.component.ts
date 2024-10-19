db20f777e6f1766bc5d9f6975c7ac4a19a06fb83
Wenying Chang <changwenyingnl@gmail.com>
2024-10-19 22:19:49 +0200

    fix:/ parent-child relationship of product-card and webshop page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SessionService } from './services/session.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent,
+    MatSnackBarModule
+  ],
   template: `
     <main class="main">
       <app-navbar></app-navbar>
@@ -18,26 +22,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: `./app.component.scss`,
 })
 export class AppComponent {
+
   title = 'client';
   constructor(
     private httpClient: HttpClient,
-    private sessionService: SessionService
   ) {}
-
-  /*
-  async ngOnInit() {
-    await this.initilizeSession();
-  }
-
-  async initilizeSession() {
-    try {
-      const sessionId = await this.sessionService.getSessionId();
-      console.log("Session ID: ", sessionId);
-    } catch (error) {
-      console.error("Error initializing session:", error);
-    }
-  }
-    */
 
 }
 
--- client/src/app/components/product-card/product-card.component.ts
+++ client/src/app/components/product-card/product-card.component.ts
@@ -1,12 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Product } from '../../interfaces/product.interface';
-import { BasketService } from '../../services/basket.service';
-import { AuthService } from '../../services/auth.service';
-import { ProductService } from '../../services/product.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { catchError, filter, of, switchMap, tap } from 'rxjs';
 
 
 @Component({
@@ -24,7 +19,7 @@ import { catchError, filter, of, switchMap, tap } from 'rxjs';
           <div class="product-card-subheader-price">€{{ product.price }}</div>
           <div class="product-card-subheader-bottom">
               <div class="product-card-subheader-bottom-stock">{{ product.stock }}</div>
-              <button class="product-card-subheader-bottom-button" (click)="addToBasket()" [disabled]="isAddingToBasket">
+              <button class="product-card-subheader-bottom-button" (click)="onAddToBasket()" [disabled]="isAddingToBasket">
                   <mat-icon mat-mini-fab>add_shopping_cart</mat-icon>
               </button>
           </div>
@@ -35,37 +30,14 @@ import { catchError, filter, of, switchMap, tap } from 'rxjs';
 })
 export class ProductCardComponent {
   @Input() product!: Product;
-  isAddingToBasket = false;
+  @Input() isAddingToBasket = false;
+  @Output() addToBasket = new EventEmitter<Product>();
+  
 
   constructor(
-    private productService: ProductService,
-    private basketService: BasketService,
-    private authService: AuthService,
-    private snackBar: MatSnackBar,
   ) {}
 
-  addToBasket() {
-    const productId = this.product._id;
-    const item = { productId, quantity: 1 };
-    this.isAddingToBasket = true;
-    this.authService.currentUser.pipe(
-      tap( user => {
-          if(!user) {
-            this.snackBar.open("Please log in to add items to your basket", 'Close', { duration: 3000 })
-          }
-        }
-      ),
-      filter(user => !!user),
-      switchMap( user => this.basketService.addToBasket(user._id, item)),
-      catchError(error  => {
-        console.error('Error adding to basket:', error);
-        this.snackBar.open('Failed to add item to basket', 'Close', { duration: 3000 });
-        return of(null); // Return null for potential side effects
-      }),
-      tap(() => {
-        this.isAddingToBasket = false,
-        this.snackBar.open('Item added to basket', 'Close', { duration: 3000 });
-      })
-    ).subscribe();
+  onAddToBasket() {
+    this.addToBasket.emit(this.product);
   }
 }
